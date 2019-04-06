539b3b1f639b2f4ba8451eff1baa0ee4cc60d20e
asharma13524 <asharma13524@gmail.com>
2019-04-06 10:12:12 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1OWzz7rM_iQkNIT_rBM8ID4gN3XyISs29'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1g8Bsp5qaDsAvjJXqfsv9oOR2f041o3Gx'
+export_file_name = 'stage-2.pth'
 
 classes = ['tennis', 'soccer']
 path = Path(__file__).parent
