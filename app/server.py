6e68f1746fbeaac1fc94e0091434117aa39ec4ef
asharma13524 <asharma13524@gmail.com>
2019-04-05 20:57:06 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/3ms7zckws46qzid/export.pkl?dl=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1OWzz7rM_iQkNIT_rBM8ID4gN3XyISs29'
 export_file_name = 'export.pkl'
 
 classes = ['tennis', 'soccer']
